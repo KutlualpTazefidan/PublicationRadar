32209ad6e3a93d39fa6161669228e3107ee50ef4
Kutlualp Tazefidan <101967979+KutlualpTazefidan@users.noreply.github.com>
2024-03-02 20:33:22 +0100

    Revert "Prediction section"

--- src/components/ClusterSection.js
+++ src/components/ClusterSection.js
@@ -1,14 +1,39 @@
 'use client'
 import { useState } from 'react'; // Import useState hook
 import styles from "./ClusterSection.module.css";
+import { z } from "zod"
+import {useForm} from "react-hook-form" 
+import { zodResolver } from "@hookform/resolvers/zod"
 
 import { Button } from "@/components/ui/button"
  
 import dynamic from 'next/dynamic';
 const My3DChartNoSSR = dynamic(() => import('./My3DScatterPlot'), { ssr: false });
 
+const formSchema = z.object({
+    // Assuming the first two fields for years are named 'year1' and 'year2'
+    year1: z.coerce.number()
+    .min(1910, { message: "Year must be between 1910 and 2024." })
+    .max(2024, { message: "Year must be between 1910 and 2024." }),
+    year2: z.coerce.number()
+    .min(1910, { message: "Year must be between 1910 and 2024." })
+    .max(2024, { message: "Year must be between 1910 and 2024." }),
+    // The third field for the string, assuming it's 'someStringField'
+    journal: z.string().min(2, {
+    message: "Field must be at least 2 characters.",
+    }),
+})
 
 const ClusterSection=()=>{
+
+  const form = useForm({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        year1: 1910, // Default value for year1
+        year2: 1910, // Default value for year2
+        journal: "Science", // Default value for journal
+      },
+    })
     
   async function onSubmit(values) {
     // console.log("values:",values)
